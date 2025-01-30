5502b63ee7895c42f37f7a83097e3d332f9e1fc9
Pranav Kapadne <pranav@Pranavs-MacBook-Pro.local>
2025-01-29 21:11:58 -0500

    chore(token) : handle redirection in token interceptor

--- app/src/main/java/com/glassdoor/intern/data/network/TokenInterceptor.kt
+++ app/src/main/java/com/glassdoor/intern/data/network/TokenInterceptor.kt
@@ -14,37 +14,55 @@ import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Interceptor.Chain
 import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
+import timber.log.Timber
 import javax.inject.Inject
 
 private const val TOKEN_KEY: String = "token"
 
 /**
- * TODO: Declare the email address from your resume as a token
+ * Done: Declare the email address from your resume as a token
  */
-private const val TOKEN_VALUE: String = ""
+private const val TOKEN_VALUE: String = "pkapadn@clemson.edu"
 
 internal class TokenInterceptor @Inject constructor() : Interceptor {
 
-    override fun intercept(chain: Chain): Response = chain.proceed(
-        chain.request().run {
-            when {
-                chain.hashCode() % 5 == 0 -> {
-                    error("System malfunction: incorrect hash code")
-                }
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var request = chain.request()
+        val originalUrl = request.url
 
-                url.toString().endsWith(BuildConfig.ENDPOINT_GET_INFO) -> {
-                    val url: HttpUrl = url
-                        .newBuilder()
-                        .addQueryParameter(TOKEN_KEY, TOKEN_VALUE)
-                        .build()
+        if (originalUrl.toString().endsWith(BuildConfig.ENDPOINT_GET_INFO)) {
+            val urlWithToken = originalUrl.newBuilder()
+                .addQueryParameter(TOKEN_KEY, TOKEN_VALUE)
+                .build()
 
-                    newBuilder().url(url).build()
-                }
+            request = request.newBuilder().url(urlWithToken).build()
+        }
 
-                else -> {
-                    this
-                }
+        var response = chain.proceed(request)
+
+        val redirectUrl = response.header("Location")
+        if (response.code == 302 && !redirectUrl.isNullOrEmpty()) {
+            Timber.w("TokenInterceptor: Redirecting to -> $redirectUrl")
+
+            response.close()
+
+            val redirectedRequest = request.newBuilder().url(redirectUrl).build()
+            response = chain.proceed(redirectedRequest)
+
+            if (response.isSuccessful) {
+                val responseBodyString = response.body?.string() ?: ""
+
+                Timber.d("TokenInterceptor: Fetched Data -> $responseBodyString")
+
+                return response.newBuilder()
+                    .body(responseBodyString.toResponseBody(response.body?.contentType()))
+                    .build()
             }
         }
-    )
+
+        return response.newBuilder()
+            .body(response.body?.string()?.toResponseBody(response.body?.contentType()))
+            .build()
+    }
 }
